da479c7cb98c65d522d2ffedbd452d9cf98f27e1
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-12-10 23:36:38 -0600

    Implementación de repository

--- app/src/main/java/mx/edu/utez/morelosgo/data/network/dao/FaovoritoDao.kt
+++
@@ -1,4 +0,0 @@
-package mx.edu.utez.morelosgo.data.network.dao
-
-class FaovoritoDao {
-}
--- app/src/main/java/mx/edu/utez/morelosgo/data/network/repository/UsuarioRepository.kt
+++ app/src/main/java/mx/edu/utez/morelosgo/data/network/repository/UsuarioRepository.kt
@@ -1,4 +1,27 @@
 package mx.edu.utez.morelosgo.data.network.repository
 
-class UsuarioRepository {
+import android.content.Context
+import mx.edu.utez.morelosgo.data.network.api.UsuarioAPI
+import mx.edu.utez.morelosgo.data.network.dao.UsuarioDao
+import mx.edu.utez.morelosgo.data.network.model.Usuario
+
+class UsuarioRepository(context: Context) : UsuarioDao {
+
+    private val api = UsuarioAPI(context)
+
+    override fun create(
+        usuario: Usuario,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit
+    ) {
+        api.create(usuario, onSuccess, onError)
+    }
+
+    override fun login(
+        usuario: Usuario,
+        onSuccess: (Usuario) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        api.login(usuario, onSuccess, onError)
+    }
 }
